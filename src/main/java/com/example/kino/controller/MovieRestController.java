7d86ae2f4124ee0facc2e06b9d9686fc4a28d73d
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-10-03 11:47:31 +0200

    Autowired
    tilføjet Autowired til respositories

--- src/main/java/com/example/kino/controller/MovieRestController.java
+++ src/main/java/com/example/kino/controller/MovieRestController.java
@@ -1,5 +1,7 @@
 package com.example.kino.controller;
 
+import com.example.kino.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class MovieRestController
 {
-
+    @Autowired
+    MovieRepository movieRepository;
 
 
 
--- src/main/java/com/example/kino/controller/ScheduleRestController.java
+++ src/main/java/com/example/kino/controller/ScheduleRestController.java
@@ -1,5 +1,7 @@
 package com.example.kino.controller;
 
+import com.example.kino.repositories.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ScheduleRestController
 {
+    @Autowired
+    ScheduleRepository scheduleRepository;
 }
