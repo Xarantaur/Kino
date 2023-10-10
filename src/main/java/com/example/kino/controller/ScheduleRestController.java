8fe0155719c673db510b4cbf061cff815d25b04f
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-10-10 13:38:32 +0200

    Get schedules endpoint

--- src/main/java/com/example/kino/controller/ScheduleRestController.java
+++ src/main/java/com/example/kino/controller/ScheduleRestController.java
@@ -1,9 +1,14 @@
 package com.example.kino.controller;
 
+import com.example.kino.model.Schedule;
 import com.example.kino.repositories.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -11,4 +16,11 @@ public class ScheduleRestController
 {
     @Autowired
     ScheduleRepository scheduleRepository;
+
+    @GetMapping("/schedule/{movie_id}")
+    public List<Schedule> getScheduleForMovie(@PathVariable int movie_id)
+    {
+        List<Schedule> schedules = scheduleRepository.findByMovieId(movie_id);
+        return schedules;
+    }
 }
