9d6c4aa3b352f7709902d9e1f988769b2cc66812
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-10-10 15:19:12 +0200

    ændringer til seat so far
    virker ikke endnu

--- src/main/java/com/example/kino/controller/SeatRestController.java
+++ src/main/java/com/example/kino/controller/SeatRestController.java
@@ -2,18 +2,22 @@ package com.example.kino.controller;
 
 import com.example.kino.model.Seat;
 import com.example.kino.repositories.SeatRepository;
+import com.example.kino.service.SeatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping()
 public class SeatRestController
 {
 
-
+    @Autowired
+    SeatService seatService;
     @Autowired
     SeatRepository seatRepository;
 
@@ -26,8 +30,38 @@ public class SeatRestController
     return seatRepository.findAll();
     }
 
-    @PostMapping("/saveseats")
-    public Seat createSeat(@RequestBody Seat seat){
-        return seatRepository.save(seat);
+    @PostMapping("/save/seats")
+    public ResponseEntity<String> saveSelectedSeats(@RequestBody List<String> selectedSeats){
+
+        try{
+            System.out.println("Received selected seats: " + selectedSeats);
+
+            List<Seat> seatsToSave = new ArrayList<>();
+
+            for(String seatInfo : selectedSeats){
+                String[] parts = seatInfo.split("-");
+                if(parts.length !=2) {
+                    return ResponseEntity.badRequest().body("Invalid seat number" + seatInfo);
+                }
+                String seatRow = parts[0];
+                String seatNumber = parts[1];
+
+                Seat seat = new Seat();
+                // midlertidig:
+                seat.setSeatId(0);
+                seat.setScheduleId(0);
+                seat.setScreenId(0);
+                // midlertidig ^
+                seat.setseatRow(seatRow);
+                seat.setSeatNumber(seatNumber);
+                seatService.saveSeat(seat);
+
+            }
+            return ResponseEntity.ok("Selected seats Saved.");
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error saving selected Seats");
+        }
+
     }
 }
--- src/main/java/com/example/kino/model/Seat.java
+++ src/main/java/com/example/kino/model/Seat.java
@@ -1,17 +1,41 @@
 package com.example.kino.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Seat
 {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long seatId;
-
+    private String seatNumber;
+    private String seatRow;
     private int scheduleId;
 
     private int screenId;
+
+    public String getSeatNumber()
+    {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber)
+    {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getseatRow()
+    {
+        return seatRow;
+    }
+
+    public void setseatRow(String seatRow)
+    {
+        this.seatRow = seatRow;
+    }
 
     public Seat(){
 
